6ec23b8bb691aa9cc18942a4b40d61bc91303d82
liufengyun <liufengyunchina@gmail.com>
2011-11-19 21:52:13 +0800

    add call-taxi test

--- spec/call_taxi_spec.rb
+++ spec/call_taxi_spec.rb
@@ -13,13 +13,63 @@ describe 'call taxi' do
     @driver.update_driver_location(119.3434, 33.5656)
   end
 
-  it "passenger should be able to get near taxi" do
-    data = { "json_data" => { latitude: 34.545, longitude: 118.324 }.to_json }
+  it "should be able for passenger to get near taxi" do
+    data = { :json_data => { latitude: 34.545, longitude: 118.324 }.to_json }
     res = @passenger.get '/passenger/taxi/near', data
 
     res.status.should == 0
     res.taxis.size.should > 0
     res.taxis[0].phone_number.should == @driver.phone_number
   end
+
+  it "should be able for passenger to send taxi call" do
+    @call_taxi_req = {
+      :json_data => {
+        to: @driver.phone_number, 
+        from: @passenger.phone_number, 
+        type: "call-taxi", 
+        data: { 
+          passenger: {
+            phone_number: "1384323242",
+            nickname: "liufy",
+            longitude: 118.23432,
+            latitude: 32.4343
+          }
+        }
+      }.to_json
+    }
+    res = @passenger.post '/passenger/message', @call_taxi_req
+    res.status.should == 0
+  end
+
+  it "should be able for driver to receive taxi call" do
+    res = @driver.get '/driver/refresh'
+    res.status.should == 0
+    res.messages?.should be_true
+    res.messages[0].should == @call_taxi_req[:json_data]
+  end
+
+  it "should be able for driver to reply taxi call" do
+    @call_taxi_replay = {
+      :json_data => {
+        to: @passenger.phone_number,
+        from: @driver.phone_number,
+        type: "call-taxi-reply",
+        data: {
+          accept: true
+        }
+      }.to_json
+    }
+    res = @driver.post '/driver/message', @call_taxi_reply
+    res.status.should == 0
+  end
+
+  it "should be able for passenger to receive taxi call reply" do
+    res = @passenger.get '/passenger/refresh'
+    res.status.should == 0
+    res.messages?.should be_true
+    res.messages[0].should == @call_taxi_reply[:json_data]
+  end
+
 end
 
